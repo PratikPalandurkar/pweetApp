0583f90222d404c3b82dc892b6778a4e5dcd0d37
Pratikpalandurkar <pratik@4ibiz.in>
2026-04-27 16:18:11 +0530

    changesdws

--- app/signUpPage/page.jsx
+++ app/signUpPage/page.jsx
@@ -96,7 +96,7 @@ const page = () => {
                   className="btn text-primary border w-100 rounded-pill my-1 "
                   onClick={() => SetSignInModal(true)}
                 >
-                  Sign Up Here
+                  Please Sign Up Here
                 </button>
               </div>
             </div>
